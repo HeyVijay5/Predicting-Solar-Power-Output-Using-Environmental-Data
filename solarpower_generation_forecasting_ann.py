b526bbb64e6ae40f430fa0d110c6e3af3e93c357
Vijay_bharadwaj <144220923+HeyVijay5@users.noreply.github.com>
2024-12-23 22:21:32 +0530

    Update solarpower_generation_forecasting_ann.py

--- solarpower_generation_forecasting_ann.py
+++ solarpower_generation_forecasting_ann.py
@@ -1,19 +1,4 @@
-# ---
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
-#       jupytext_version: 1.6.0
-#   kernelspec:
-#     display_name: Python 3
-#     name: python3
-# ---
 
-# %% [markdown] id="UicUNHNs7wGC"
-# # Solar Power Generation Forecasting By ANN
-#
 #
 
 # %% [markdown] id="EUBRdZEs_Ucx"
